3d5197f1809d000822c96c1468fa2905cc541c53
ii2Chris <ChrisBudhram03@outlook.com>
2025-10-03 10:48:05 -0400

    Added some comments

--- stats.py
+++ stats.py
@@ -1,4 +1,5 @@
 def length_of_text(text):
+    # split the text into words
     text_length = []
     for word in text.split():
         text_length.append(word)
@@ -6,6 +7,7 @@ def length_of_text(text):
 
 
 def get_chars_dict(text):
+    # create a dictionary of characters and their counts
     chars = {}
     for c in text:
         lowered = c.lower()
@@ -16,10 +18,12 @@ def get_chars_dict(text):
     return chars
 
 def sort_on(item):
+    # item is a tuple (char, count)
     return item[1]
 
 def sort_chars(chars):
+    # [('a', 10), ('b', 5), ('c', 20)]
     items = list(chars.items())
-    
+
     sorted_items = sorted(items, key=sort_on, reverse=True)
     return sorted_items
